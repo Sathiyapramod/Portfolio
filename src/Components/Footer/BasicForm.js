0800bf949f8eccd61a3c71c6b614bff92b56cf4a
Sathiyapramod <sathiyapramod22@gmail.com>
2023-03-22 18:55:54 +0530

    added emailing services

--- src/Components/Footer/BasicForm.js
+++ src/Components/Footer/BasicForm.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "../Footer/Footer.css";
 import { Paper } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 function BasicForm(props) {
+  
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_PUBLIC_KEY
+      )
+      .then((result)=>console.log(result),(error)=>console.log(error));
+  };
   return (
     <div>
       <Paper
@@ -15,10 +29,11 @@ function BasicForm(props) {
         elevation={6}
         className="basicform"
       >
-        <form className="d-flex flex-column gap-3">
+        <form className="d-flex flex-column gap-3" ref={form} onSubmit={sendEmail}>
           <div className="d-flex flex-row gap-3 align-items-center">
             <TextField
               variant="standard"
+              name="user_name"
               label={props.basicdata.formdata.name}
               size="small"
               fullWidth
@@ -27,6 +42,7 @@ function BasicForm(props) {
           <div className="d-flex flex-row gap-3 align-items-center">
             <TextField
               variant="standard"
+              name="user_email"
               label={props.basicdata.formdata.email}
               size="small"
             />
@@ -34,6 +50,7 @@ function BasicForm(props) {
           <div className="d-flex flex-row gap-3 align-items-center">
             <TextField
               variant="standard"
+              name="user_message"
               label={props.basicdata.formdata.message}
               multiline
               rows={4}
@@ -43,6 +60,8 @@ function BasicForm(props) {
           <div>
             <Button
               variant="contained"
+              type="submit"
+              value="Send"
               color="primary"
               sx={{
                 backgroundColor: "#756BEE",
